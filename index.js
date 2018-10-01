7eda9362400e12ac1a28c65d093b3bdaa466a06b
Nate Miller <natekmiller96@gmail.com>
2018-10-01 04:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const logDriversByHometown = function (array, loc){
 };
 
 const driversByRevenue = function(array){
-  array.sort(function (a,b){
+  newArray = array;
+  newArray.sort(function (a,b){
     return a.revenue - b.revenue;
   });
-  return array;
+  return newArray;
 };
