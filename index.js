17235eb767378b24f106bfcdce884062f6c6a440
Nate Miller <natekmiller96@gmail.com>
2018-10-01 04:47:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const driversByName = function(array){
 };
 
 const totalRevenue = function(array){
-  return array.reduce(function (accumulator, c){
-    return  + currentValue;
+  return array.reduce(function (accumulator, currentValue){
+    return  accumulator + currentValue;
   });
 };
