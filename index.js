f56d201dbfebe396cb2995440370305be01f0e4f
Nate Miller <natekmiller96@gmail.com>
2018-10-01 03:51:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const logDriverNames = function (array){
     console.log(array.name);
   });
 };
+
+const logDriversByHometown = function (array, loc){
+  array.forEach(function (array) {
+    if (array.location === loc){
+      console.log(array.name);
+    }
+  });
+};
